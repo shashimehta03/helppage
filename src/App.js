f2ff14760d951cb44c40ce21f2b51c956593b01b
Shashi Mehta <103140467+shashimehta03@users.noreply.github.com>
2023-01-12 17:26:56 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1 className="nav"> </h1>
+      <img className="top" src="Help_page.PNG" alt="" />
+      <hr className="line" />
+      <hr className="fatline" />
+      <h1 className="mock">
+        <a href="#MOOCs">MOOCs</a>
+      </h1>
+      <h1 className="gp">
+        <a href=" ">General Proficiency</a>
+      </h1>
+      <h1 className="coursera">
+        <a href="#coursera">Coursera</a>
+      </h1>
+      <h1 className="outlook">
+        <a href="#Outlook">Outlook</a>
+      </h1>
+      <h1 className="allbox"> </h1>
+      <h1 className="allhalf"> </h1>
+      <h1 className="enroll">Q. How to Enroll in MOOCs courses ?</h1>
+      <div className="answer">
+        {" "}
+        <br /> Answer:
+        <br />
+        You will have to Enroll MOOC courses through NPTEL. <br />
+        Search for a course as per your field and interest and enroll.
+        <br /> Follow this Link to select Courses: <br />
+        <button href="  ">Click Here!</button>
+      </div>
+      {/*second box*/}
+      <h1 className="allbox2"> </h1>
+      <h1 className="allhalf2"> </h1>
+      <h1 className="enroll2">Q. How to Enroll in MOOCs courses ?</h1>
+      <div className="answer2">
+        {" "}
+        <br /> Answer:
+        <br />
+        You will have to Enroll MOOC courses through NPTEL. <br />
+        Search for a course as per your field and interest and enroll.
+        <br /> Follow this Link to select Courses: <br />
+        <button href=" ">Click Here!</button>
+      </div>
+      <footer>
+        <h3> </h3>
+      </footer>
+    </div>
+  );
+}
